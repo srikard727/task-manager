96b637ccf7a26ea84e04a3da1c42f63be9dfff84
SirPopGhost <47957443+SirPopGhost@users.noreply.github.com>
2026-06-02 22:51:36 -0700

    Pushing what I have so far

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({mssg: "Wel"})
+})
+
+app.listen(4000, () => {
+    console.log("Listening on port 4000!!")
+})
+
