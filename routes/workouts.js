beccbeba337c0287d6d32ce45ab92478b1143278
SirPopGhost <47957443+SirPopGhost@users.noreply.github.com>
2026-06-06 15:32:32 -0700

    Minimal push

--- routes/workouts.js
+++ routes/workouts.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', () => {})
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all workouts'})
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 
 
 // routes
-app.use('/api/workouts', workoutRoutes)
+app.use('/api/workouts/', workoutRoutes)
 
 // listener for requests
 app.listen(process.env.PORT, () => {
