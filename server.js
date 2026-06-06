12161188a481c3913a9c2b7b32ce00d29fad539f
SirPopGhost <47957443+SirPopGhost@users.noreply.github.com>
2026-06-05 22:12:19 -0700

    express.Router for modular routers

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts.js')
 
+// express app
 const app = express()
 
-app.get("/", (req, res) => {
-    
+// middleware
+app.use((req, res, next) => {   
+    console.log(req.path, req.method)
+    next()
 })
 
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// listener for requests
 app.listen(process.env.PORT, () => {
     console.log("Listening on port", process.env.PORT)
 })
